7e0b4fbe41f948bf8597c74b2586ab8cd942e47e
yubiyun <245817300@qq.com>
2023-02-11 17:30:12 +0800

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongoService } from './common/mongo/mongo.service';
+import { RedisService } from './common/redis/redis.service';
+import { MqService } from './common/mq/mq.service';
+import { ProducerService } from './chat/producer/producer.service';
+import { ConsumerService } from './chat/consumer/consumer.service';
+import { CqhttpController } from './chat/cqhttp/cqhttp.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CqhttpController],
+  providers: [AppService, MongoService, RedisService, MqService, ProducerService, ConsumerService],
 })
-export class AppModule {}
+export class AppModule { }
