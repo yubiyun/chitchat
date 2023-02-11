626c563a9d4f1a91d036ecda6a6c46a60989948b
liyu <liyu@iftech.io>
2023-02-11 23:53:10 +0800

    feat(qbot): log message

--- src/chat/qbot/qbot.service.ts
+++ src/chat/qbot/qbot.service.ts
@@ -1,12 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { GroupMessage } from './types/group-message';
 import { PrivateMessage } from './types/private-message';
 
 @Injectable()
 export class QbotService {
-  async handlePrivateMessage(payload: PrivateMessage) {}
+  private readonly logger = new Logger(QbotService.name);
 
-  async handleGroupMessage(payload: GroupMessage) {}
+  async handlePrivateMessage(payload: PrivateMessage) {
+    this.logger.log({
+      from: payload.user_id,
+      to: payload.self_id,
+      msg: payload.raw_message,
+      nickname: payload.sender.nickname,
+    });
+  }
+
+  async handleGroupMessage(payload: GroupMessage) {
+    this.logger.log({
+      group: payload.group_id,
+      from: payload.user_id,
+      to: payload.self_id,
+      msg: payload.raw_message,
+      nickname: payload.sender.nickname,
+    });
+  }
 
   async sendMessage() {}
 }
